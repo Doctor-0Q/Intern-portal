6d0f41f7385e057cadf1ae293642d4aa06003181
Aman Singh <amansingh10122003@gmail.com>
2024-09-19 14:18:20 +0530

    Update AdminPage.jsx

--- Client/src/Pages/AdminPage.jsx
+++ Client/src/Pages/AdminPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import toast from 'react-hot-toast'
+import API_URL from '../config';
 
 export const AdminPage = () => {
   const [email,setEmail]=useState("")
