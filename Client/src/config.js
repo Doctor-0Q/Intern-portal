428b3e0f793ba69f488572c5345ebb1e99a76c22
Aman Singh <amansingh10122003@gmail.com>
2024-09-19 18:24:10 +0530

    Update config.js

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,2 +1,2 @@
-const API_URL = import.meta.env.VITE_API_URL || 'intern-portal-six.vercel.app/api/v1';
+const API_URL = import.meta.env.VITE_API_URL || 'https://intern-portal-six.vercel.app/api/v1';
 export default API_URL;
