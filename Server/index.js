1baf66c436a1ee9be7c1eb819e1f40e2cf29d920
Aman Singh <amansingh10122003@gmail.com>
2024-09-19 13:12:33 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -13,17 +13,15 @@ import certificateRoute from './Routes/certificate.route.js'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',  
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
   credentials: true  
 }));
-
-
 
 connection('test');  
 cleanupTempFolder();
@@ -45,5 +43,7 @@ app.listen(port, () => {
 app.use((req,res)=>{
   console.log("request received");
 });
+
+export default app;
 
 
